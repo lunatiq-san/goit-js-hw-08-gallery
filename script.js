56501985f1749dd066750f15ce736472cd87fbce
lunatiq-san <lunatiq86@gmail.com>
2021-04-19 15:16:05 +0300

    Finished hw-08-gallery

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 import images from './gallery-items.js';
 
-// + Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// + Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-// + Открытие модального окна по клику на элементе галереи.
-// + Подмена значения атрибута src элемента img.lightbox__image.
-// + Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-// + Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
 const refs = {
   imagesContainer: document.querySelector('.js-gallery'),
   modalContainer: document.querySelector('.lightbox'),
   openLightboxImage: document.querySelector('.lightbox__image'),
 };
 
-let activeIndex = 0;
-
 const imagesMarkup = createGaleryItemMarkup(images);
 
-// 1. Создать разметку
 refs.imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup.join(''));
+refs.imagesContainer.addEventListener('click', onImageGalleryClick);
 
 function createGaleryItemMarkup(images) {
   return images.map(({ preview, original, description }) => {
-    // imagesSrc.push(`${original}`);
     return `
   <li class="gallery__item">
     <a
@@ -39,13 +30,6 @@ function createGaleryItemMarkup(images) {
   `;
   });
 }
-// 2. Повесить слушателя событий на контейнер с будущей разметкой
-// 2.1 Если нажали не на картинку - пропускаем
-// 2.2 Получение url большого изображения
-// 2.3 Открытие модального окна по клику на элементе галереи
-// 2.4 Подмена значения атрибута src элемента img.lightbox__image
-
-refs.imagesContainer.addEventListener('click', onImageGalleryClick);
 
 function onImageGalleryClick(event) {
   event.preventDefault();
@@ -57,29 +41,35 @@ function onImageGalleryClick(event) {
   const galleryImageSrcRef = event.target.dataset.source;
 
   refs.modalContainer.classList.add('is-open');
-  refs.openLightboxImage.src = galleryImageSrcRef;
+  refs.modalContainer.addEventListener('click', onCloseModalByClick);
 
-  // 3. + Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
-  // 3.1 + Закрытие модального окна по клику на div.lightbox__overlay.
+  document.addEventListener('keydown', onCloseModalByEscape);
 
-  refs.modalContainer.addEventListener('click', onCloseModal);
+  changeImageSrc(galleryImageSrcRef);
+  onCloseModalByClick(event);
+  onCloseModalByEscape(event);
+}
 
-  function onCloseModal(event) {
-    if (event.target.nodeName === 'IMG') {
-      return;
-    }
-
-    refs.modalContainer.classList.remove('is-open');
-    refs.openLightboxImage.src = '';
+function onCloseModalByClick(event) {
+  if (event.target.nodeName === 'IMG') {
+    return;
   }
 
-  // 3.2 - Закрытие модального окна по нажатию клавиши ESC.
-  document.addEventListener('keydown', onCloseModalEscape);
+  refs.modalContainer.classList.remove('is-open');
 
-  function onCloseModalEscape(event) {
-    if (event.key === 'Escape') {
-      onCloseModal(event);
-      console.log(event);
-    }
+  clearImageSrc();
+}
+
+function onCloseModalByEscape(event) {
+  if (event.key === 'Escape') {
+    onCloseModalByClick(event);
   }
+}
+
+function changeImageSrc(source) {
+  refs.openLightboxImage.src = source;
+}
+
+function clearImageSrc() {
+  refs.openLightboxImage.src = '';
 }
